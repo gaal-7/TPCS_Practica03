b84804c94e40a9f0145fa32c504991a63ce03a33
gaal <131891384+gaal-7@users.noreply.github.com>
2023-10-13 17:13:39 -0600

    Update HibernateUtil.java

--- src/main/java/org/uv/tpcs_practica03/HibernateUtil.java
+++ src/main/java/org/uv/tpcs_practica03/HibernateUtil.java
@@ -17,17 +17,16 @@ public class HibernateUtil {
             Configuration configuration = new Configuration();
 
             settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/ventas"); // Reemplaza "your_database_name" con el nombre de tu base de datos.
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/ventas");
             settings.put(Environment.USER, "root");
-            settings.put(Environment.PASS, ""); // Reemplaza "your_password" con la contraseña de tu base de datos.
+            settings.put(Environment.PASS, ""); 
 
             settings.put(Environment.SHOW_SQL, "true");
             settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-            settings.put(Environment.HBM2DDL_AUTO, "update"); // Cambiado a "update" para no perder datos en cada reinicio.
+            settings.put(Environment.HBM2DDL_AUTO, "update"); 
 
             configuration.setProperties(settings);
 
-            // Agregar los POJOs como entidades
             configuration.addAnnotatedClass(Cliente.class);
             configuration.addAnnotatedClass(Venta.class);
             configuration.addAnnotatedClass(DetalleVenta.class);
